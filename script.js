049246d59819764fa48b66d331455e03995c9c2c
Felipe Galego <felipegalegoosawa@gmail.com>
2022-02-05 21:34:44 -0300

    code refactoring (destructuring from fetch api)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.pokemon-container')
 const generate = document.querySelector('.pokemon-generate')
 const totalPokemon = 898
-const colors = {
+const colorsByType = {
   fire: '#FC7D25',
   grass: '#9ACD50',
   electric: '#FCF7DE',
@@ -22,47 +22,44 @@ const colors = {
   dark: '#707070'
 }
 
-const pokemon_types = Object.keys(colors)
+const pokemonTypesColor = Object.keys(colorsByType)
+
+const loading = () => {
+  let loader = `<div class="loading"></div>`
+  container.innerHTML = loader
+}
 
 const getPokemon = async _ => {
+  loading()
   const randomPokemon = Math.floor(Math.random() * totalPokemon)
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`
   )
-  const data = await response.json()
-  showPokemon(data)
+  const { id, name, types } = await response.json()
+  showPokemon(id, name, types)
 }
 
-const showPokemon = pokemon => {
+const showPokemon = (id, name, types) => {
   container.innerHTML = ''
-  // Preenchendo com 0
-  const id = pokemon.id.toString().padStart(3, '0')
-  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-  // Pegando o nome/ o tipo
-  const typeName = pokemon.types.map(type => type.type.name)
-  // console.log(typeName)
-  const type = pokemon_types.find(type => typeName.indexOf(type) > -1)
-  // console.log(type)
-  const color = colors[type]
-  // console.log(color)
-  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}`
-
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}`
+  const type = types.map(type => type.type.name)
+  const primaryType = pokemonTypesColor.find(type => type.indexOf(type) > -1)
+  const color = colorsByType[primaryType]
   const output = `
     <div class="pokemon-img">
-      <img src="${img}.png"" alt="${name}">
+    <img src="${img}.png"" alt="${name}">
     </div>
     <div class="pokemon-info">
-      <span class="pokemon-number">#${id}</span>
-      <h3 class="pokemon-name">${name}</h3>
-      <small class="pokemon-type">${
-        type[0].toUpperCase() + type.slice(1)
-      }</small>
+    <span class="pokemon-number">#${id.toString().padStart(3, '0')}</span>
+    <h3 class="pokemon-name">${name[0].toUpperCase() + name.slice(1)}</h3>
+    <small class="pokemon-type">${type[0].toUpperCase() + type.slice(1)}</small>
     </div>
-  `
+    `
   container.style.backgroundColor = color
   generate.style.backgroundColor = color
   container.innerHTML += output
 }
 
+loading()
 getPokemon()
 generate.addEventListener('click', getPokemon)
